516c769d5a995bb93599f201ac48f0f003326c58
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2022-06-07 07:25:36 +0200

    Add Login page

---
+++ BADSYS/BADSYS/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BADSYS.Pages.UserManagement
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

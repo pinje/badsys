abd3957e8b92745e84fd24695884917fd10fdce9
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2022-06-09 10:52:57 +0200

    Add match result to profile

--- BADSYS/BADSYS/Pages/Profile.cshtml.cs
+++ BADSYS/BADSYS/Pages/Profile.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Models;
 using DLL;
 using DAL.UserBranch;
+using DAL.ParticipationBranch;
+using DAL.TournamentBranch;
 
 namespace BADSYS.Pages
 {
@@ -10,10 +12,30 @@ namespace BADSYS.Pages
     {
         [BindProperty]
         public User User { get; set; }
+        [BindProperty]
+        public List<Participation> Participations { get; set; }
+        [BindProperty]
+        public List<Tournament> TournamentsInformation { get; set; }
+
         public void OnGet(int userid)
         {
+            // get user info
             UserManager userManager = new UserManager(new UserDAL());
             User = userManager.GetUser(userid);
+
+            // get user participations 
+            ParticipationManager participationManager = new ParticipationManager(new ParticipationDAL());
+            Participations = participationManager.GetAllParticipationByPlayer(userid);
+
+            // get tournament info from the participations list
+            TournamentManager tournamentManager = new TournamentManager(new TournamentDAL());
+            TournamentsInformation = new List<Tournament>();
+
+            foreach (Participation participation in Participations)
+            {
+                Tournament tournament = tournamentManager.GetTournamentById(participation.TournamentId);
+                TournamentsInformation.Add(tournament);
+            }
         }
     }
 }
